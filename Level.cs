df175e60e26ce11c93eca96a3387cffd02b7ec88
Amit Kumar Ghosh <Amit_kumar_Ghosh@hotmail.com>
2018-10-08 17:45:21 -0700

    Level Class Updated

--- Level.cs
+++ Level.cs
@@ -6,7 +6,52 @@ using System.Threading.Tasks;
 
 namespace TowerDefense
 {
-    class Level
+    public class Level
     {
+        private readonly Invader[] _invaders;
+
+        public Tower[] Towers { get; set; }
+
+        public Level(Invader[] invaders)
+        {
+            _invaders = invaders;
+        }
+
+        // True if player wins and false if player looses
+        public bool Play()
+        {
+            // Run until all Invaders are nuetralized or an invader reaches the end of path
+            int remainingInvaders = _invaders.Length;
+
+            while (remainingInvaders > 0)
+            {
+                //Each Tower has oppurtunity to fire on Invaders
+                foreach (Tower tower in Towers)
+                {
+                    tower.FireOnInvaders(_invaders);
+                }
+
+                //Count and Move the invaders that are still active
+                remainingInvaders = 0;
+                foreach (Invader invader in _invaders)
+                {
+                    if (invader.IsActive)
+                    {
+                        invader.Move();
+                        {
+                            if(invader.HasScored)
+                            {
+                                return false;
+                            }
+                        }
+                        remainingInvaders += 1;
+                    }
+                        
+                }
+
+            }
+            return true;
+
+        }
     }
 }
